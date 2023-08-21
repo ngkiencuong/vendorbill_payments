8b5741d98bff6cfa9bf789a7f6b5f234a088cb24
ngkiencuong <85164180+ngkiencuong@users.noreply.github.com>
2023-08-21 09:08:47 +0700

    Alpha

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Vendor Payment Enhancement",
+    'summary': "Vendor Payment Enhancement Module for Odoo Version 16 Enterprise",
+    'version': '16.0.1.0.0',
+    'author': "Simplify-ERP™",
+    'category': 'Accounting',
+    'website': 'https://simplify-erp.com',
+    'license': 'LGPL-3',
+    'depends': ["account_accountant"],
+    'data': [
+        'views/account_move_views.xml'
+    ],
+    'auto_install': False,
+    'installable': True,
+}
